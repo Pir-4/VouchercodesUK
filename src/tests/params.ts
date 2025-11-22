7b9832869581c63e9b6163e1be5c401d8b6de28e
Valentin Agapitov <valentin.agapitov@exness.com>
2025-11-22 16:37:01 +0000

    Refactor params.ts for consistent formatting and correct property names in PEOPLE array; ensure uniform spacing in locations and dates arrays.

--- src/tests/params.ts
+++ src/tests/params.ts
@@ -1,91 +1,91 @@
-import {getDayShiftDate} from "./helpers"
+import { getDayShiftDate } from "./helpers"
 
 export const LOCATIONS = [
-    "N17 9QJ",
-    "n17 9qj",
-    "SW7 5ND",
+	"N17 9QJ",
+	"n17 9qj",
+	"SW7 5ND",
 ];
 export const DATES = [
-    {
-    	dateValue: "Any",
-    	dateName: "Any",
-    },
-    {
-        dateValue: "Today",
-        dateName: "Today",
-    },
-    {
-        dateValue: "Tomorrow",
-        dateName: "Tomorrow",
-    },
-    {
-        dateValue: getDayShiftDate(2),
-        dateName: "Tomorrow+1",
-    },
-    {
-        dateValue: getDayShiftDate(3),
-        dateName: "Tomorrow+2",
-    },
-    {
-        dateValue: getDayShiftDate(4),
-        dateName: "Tomorrow+3",
-    },
-    {
-        dateValue: getDayShiftDate(5),
-        dateName: "Tomorrow+4",
-    },
-    {
-        dateValue: getDayShiftDate(6),
-        dateName: "Tomorrow+5",
-    },
+	{
+		dateValue: "Any",
+		dateName: "Any",
+	},
+	{
+		dateValue: "Today",
+		dateName: "Today",
+	},
+	{
+		dateValue: "Tomorrow",
+		dateName: "Tomorrow",
+	},
+	{
+		dateValue: getDayShiftDate(2),
+		dateName: "Tomorrow+1",
+	},
+	{
+		dateValue: getDayShiftDate(3),
+		dateName: "Tomorrow+2",
+	},
+	{
+		dateValue: getDayShiftDate(4),
+		dateName: "Tomorrow+3",
+	},
+	{
+		dateValue: getDayShiftDate(5),
+		dateName: "Tomorrow+4",
+	},
+	{
+		dateValue: getDayShiftDate(6),
+		dateName: "Tomorrow+5",
+	},
 ];
 export const PEOPLE = [
-    {
-        peopeleValue: "Any",
-        dateName: "Any",
-    },
-    {
-        peopeleValue: "1",
-        dateName: "1",
-    },
-    {
-        peopeleValue: "2",
-        dateName: "2",
-    },
-    {
-        peopeleValue: "3",
-        dateName: "3",
-    },
-    {
-        peopeleValue: "4",
-        dateName: "4",
-    },
-    {
-        peopeleValue: "5",
-        dateName: "5",
-    },
-    {
-        peopeleValue: "6",
-        dateName: "6",
-    },
-    {
-        peopeleValue: "7",
-        dateName: "7",
-    },
-    {
-        peopeleValue: "8",
-        dateName: "8",
-    },
-    {
-        peopeleValue: "9",
-        dateName: "9",
-    },
-    {
-        peopeleValue: "10",
-        dateName: "10",
-    },
-    {
-        peopeleValue: "10+",
-        dateName: "10+",
-    },
+	{
+		peopleValue: "Any",
+		dateName: "Any",
+	},
+	{
+		peopleValue: "1",
+		dateName: "1",
+	},
+	{
+		peopleValue: "2",
+		dateName: "2",
+	},
+	{
+		peopleValue: "3",
+		dateName: "3",
+	},
+	{
+		peopleValue: "4",
+		dateName: "4",
+	},
+	{
+		peopleValue: "5",
+		dateName: "5",
+	},
+	{
+		peopleValue: "6",
+		dateName: "6",
+	},
+	{
+		peopleValue: "7",
+		dateName: "7",
+	},
+	{
+		peopleValue: "8",
+		dateName: "8",
+	},
+	{
+		peopleValue: "9",
+		dateName: "9",
+	},
+	{
+		peopleValue: "10",
+		dateName: "10",
+	},
+	{
+		peopleValue: "10+",
+		dateName: "10+",
+	},
 ];
