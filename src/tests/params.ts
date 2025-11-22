0b56232fcf0a271636d1737ba07a5b0582c9f087
Valentin Agapitov <valentin.agapitov@exness.com>
2025-11-22 15:43:46 +0000

    Add parameters for locations, dates, and people in tests; refactor restaurant tests to utilize these constants

---
+++ src/tests/params.ts
@@ -0,0 +1,85 @@
+export const LOCATIONS = ["N17 9QJ", "n17 9qj", "SW7 5ND"];
+export const DATES = [
+	{
+		dateValue: "Any",
+		dateName: "Any",
+	},
+	{
+		dateValue: "Today",
+		dateName: "Today",
+	},
+	{
+		dateValue: "Tomorrow",
+		dateName: "Tomorrow",
+	},
+	{
+		dateValue: new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString().split('T')[0],
+		dateName: "Tomorrow+1",
+	},
+	// {
+	// 	dateValue: "Next month",
+	// 	dateName: "Tomorrow+2",
+	// },
+	// 	{
+	// 		dateValue: "Next month",
+	// 		dateName: "Tomorrow+3",
+	// 	},
+	// 	{
+	// 		dateValue: "Next month",
+	// 		dateName: "Tomorrow+4",
+	// 	},
+	// 	{
+	// 		dateValue: "Next month",
+	// 		dateName: "Tomorrow+5",
+	// 	},
+];
+export const PEOPLE = [
+	{
+		peopeleValue: "Any",
+		dateName: "Any",
+	},
+	{
+		peopeleValue: "1",
+		dateName: "1",
+	},
+	{
+		peopeleValue: "2",
+		dateName: "2",
+	},
+	{
+		peopeleValue: "3",
+		dateName: "3",
+	},
+	{
+		peopeleValue: "4",
+		dateName: "4",
+	},
+	{
+		peopeleValue: "5",
+		dateName: "5",
+	},
+	{
+		peopeleValue: "6",
+		dateName: "6",
+	},
+	{
+		peopeleValue: "7",
+		dateName: "7",
+	},
+	{
+		peopeleValue: "8",
+		dateName: "8",
+	},
+	{
+		peopeleValue: "9",
+		dateName: "9",
+	},
+	{
+		peopeleValue: "10",
+		dateName: "10",
+	},
+	{
+		peopeleValue: "10+",
+		dateName: "10+",
+	},
+];
--- src/tests/restaurants.test.ts
+++ src/tests/restaurants.test.ts
@@ -1,12 +1,15 @@
 import { expect } from '@playwright/test';
-import {test} from "@tests/fixtures"
+import { test } from "@tests/fixtures"
+import { LOCATIONS, DATES, PEOPLE } from "@tests/params";
 
-test('has title', async ({ mainPage }) => {
-    await mainPage.open();
-    await mainPage.closePrivacyWindow();
-    const rPage = await mainPage.moveToRestaurantsPage();
-    await rPage.fillLocation('n17 9qj');
-    await rPage.selectDate('Today');
-    await rPage.selectPeople('4');
-    const t = 5;
+LOCATIONS.forEach((location) => {
+    test(`Search offers for ${location}`, async ({ mainPage }) => {
+        await mainPage.open();
+        await mainPage.closePrivacyWindow();
+        const rPage = await mainPage.moveToRestaurantsPage();
+        await rPage.fillLocation(location);
+        await rPage.selectDate('Today');
+        await rPage.selectPeople('4');
+        const t = 5;
+    });
 });
