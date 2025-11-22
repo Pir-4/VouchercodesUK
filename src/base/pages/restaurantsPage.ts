a6634ecb49b451d5396ebc7c7a874d5638da0228
Valentin Agapitov <valentin.agapitov@exness.com>
2025-11-22 14:45:41 +0000

    add select methods

--- src/base/pages/restaurantsPage.ts
+++ src/base/pages/restaurantsPage.ts
@@ -5,4 +5,19 @@ export class RestaurantsPage extends BasePage {
     public static createPage(page: Page) {
         return new RestaurantsPage(page, page.url());
     }
+
+    public async fillLocation(text: string) {
+        const locationInput = this.getById('google-autocomplete');
+        await locationInput.fill(text);
+    }
+
+    public async selectDate(value: string) {
+        const dateSelect = this.getById('day-select');
+        await dateSelect.selectOption(value);
+    }
+
+    public async selectPeople(value: string) {
+        const peopleSelect = this.getById('people-select');
+        await peopleSelect.selectOption(value);
+    }
 }
--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -5,5 +5,8 @@ test('has title', async ({ mainPage }) => {
     await mainPage.open();
     await mainPage.closePrivacyWindow();
     const rPage = await mainPage.moveToRestaurantsPage();
+    await rPage.fillLocation('n17 9qj');
+    await rPage.selectDate('Today');
+    await rPage.selectPeople('4');
     const t = 5;
 });
